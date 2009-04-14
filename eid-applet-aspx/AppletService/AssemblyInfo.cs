2700ed4ccbfb4d093faec1d7f3cee19b29c6418c
Frank Cornelis <fcorneli@users.noreply.github.com>
2009-04-14 18:00:23 +0000

    initial work on the ASP.NET eID Applet Service implementation

--- eid-applet-aspx/AppletService/AssemblyInfo.cs
+++ eid-applet-aspx/AppletService/AssemblyInfo.cs
@@ -1,8 +1,21 @@
-// AssemblyInfo.cs created with MonoDevelop
-// User: fcorneli at 2:13 PM 4/14/2009
-//
-// To change standard headers go to Edit->Preferences->Coding->Standard Headers
-//
+/*
+ * eID Applet Project.
+ * Copyright (C) 2009 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
