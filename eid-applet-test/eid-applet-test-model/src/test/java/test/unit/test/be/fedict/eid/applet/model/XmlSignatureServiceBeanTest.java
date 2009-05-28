b9382f635d64e1e3f1a87be08df283e459b62146
Frank Cornelis <fcorneli@users.noreply.github.com>
2009-05-28 11:45:58 +0000

    fix: XML signature demo can handle filenames with spaces now

--- eid-applet-test/eid-applet-test-model/src/test/java/test/unit/test/be/fedict/eid/applet/model/XmlSignatureServiceBeanTest.java
+++ eid-applet-test/eid-applet-test-model/src/test/java/test/unit/test/be/fedict/eid/applet/model/XmlSignatureServiceBeanTest.java
@@ -82,6 +82,7 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.logging.Log;
@@ -186,7 +187,10 @@ public class XmlSignatureServiceBeanTest {
 		LOG.debug("test.txt: " + new URI("test.txt"));
 		LOG.debug("1234.txt: " + new URI("1234.txt"));
 		LOG.debug("hello world.txt: "
-				+ new File(new File("."), "hello world.txt").toURI());
+				+ new File("hello world.txt").toURI().toURL().getFile());
+		LOG.debug("hello world.txt: "
+				+ FilenameUtils.getName(new File("hello world.txt").toURI()
+						.toURL().getFile()));
 	}
 
 	@Test
@@ -273,6 +277,52 @@ public class XmlSignatureServiceBeanTest {
 		boolean signatureResult = xmlSignature.checkSignatureValue(keyPair
 				.getPublic());
 		assertTrue(signatureResult);
+	}
+
+	@Test
+	public void testJsr105ReferenceUri() throws Exception {
+		String uri = FilenameUtils.getName(new File("foo bar.txt").toURI()
+				.toURL().getFile());
+
+		KeyPair keyPair = generateKeyPair();
+
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
+				.newInstance();
+		documentBuilderFactory.setNamespaceAware(true);
+		DocumentBuilder documentBuilder = documentBuilderFactory
+				.newDocumentBuilder();
+		Document document = documentBuilder.newDocument();
+
+		XMLSignatureFactory signatureFactory = XMLSignatureFactory.getInstance(
+				"DOM", new org.jcp.xml.dsig.internal.dom.XMLDSigRI());
+
+		XMLSignContext signContext = new DOMSignContext(keyPair.getPrivate(),
+				document);
+
+		byte[] externalDocument = "hello world".getBytes();
+		MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
+		messageDigest.update(externalDocument);
+		byte[] documentDigestValue = messageDigest.digest();
+
+		DigestMethod digestMethod = signatureFactory.newDigestMethod(
+				DigestMethod.SHA1, null);
+		Reference reference = signatureFactory.newReference(uri, digestMethod,
+				null, null, null, documentDigestValue);
+
+		SignatureMethod signatureMethod = signatureFactory.newSignatureMethod(
+				SignatureMethod.RSA_SHA1, null);
+		CanonicalizationMethod canonicalizationMethod = signatureFactory
+				.newCanonicalizationMethod(
+						CanonicalizationMethod.EXCLUSIVE_WITH_COMMENTS,
+						(C14NMethodParameterSpec) null);
+		javax.xml.crypto.dsig.SignedInfo signedInfo = signatureFactory
+				.newSignedInfo(canonicalizationMethod, signatureMethod,
+						Collections.singletonList(reference));
+
+		javax.xml.crypto.dsig.XMLSignature xmlSignature = signatureFactory
+				.newXMLSignature(signedInfo, null);
+
+		xmlSignature.sign(signContext);
 	}
 
 	@Test
