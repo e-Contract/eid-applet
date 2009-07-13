e1e282455a81a4e3df3f63f68584d567678b2733
Frank Cornelis <fcorneli@users.noreply.github.com>
2009-07-13 15:14:51 +0000

    ASP.NET authn signature verification

--- eid-applet-aspx/AppletService/AuthnAppletService.cs
+++ eid-applet-aspx/AppletService/AuthnAppletService.cs
@@ -22,6 +22,7 @@ using System.Web.SessionState;
 using System.IO;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 
 namespace Be.FedICT.EID.Applet.Service {
@@ -57,9 +58,12 @@ namespace Be.FedICT.EID.Applet.Service {
 				byte[] challenge = new byte[20];
 				random.NextBytes(challenge);
 				httpResponse.BinaryWrite(challenge);
+				
+				httpContext.Session.Add("Challenge", challenge);
 				return;
 			}
 			if ("AuthenticationDataMessage".Equals(messageType)) {
+				// parse arguments
 				String saltValueSizeString = httpRequest.Headers["X-AppletProtocol-SaltValueSize"];
 				String signatureValueSizeString = httpRequest.Headers["X-AppletProtocol-SignatureValueSize"];
 				int saltValueSize = Int32.Parse(saltValueSizeString);
@@ -77,12 +81,41 @@ namespace Be.FedICT.EID.Applet.Service {
 				inputStream.Read(certsData, 0, bytesLeft);
 				
 				X509Certificate2 certificate2 = new X509Certificate2(certsData);
+				Log("certs data size: " + certsData.Length);
+				Log("cert data size: " + certificate2.GetRawCertData().Length);
 				
-				// TODO: verify signature
-				RSACryptoServiceProvider rsaCryptoService = new RSACryptoServiceProvider();
-				byte[] toBeSignedData = new byte[20]; // TODO
-				//bool signatureResult = rsaCryptoService.VerifyData(toBeSignedData, CryptoConfig.MapNameToOID("SHA1"), signatureValue);
-				//Log("signature result: " + signatureResult);
+				// verify signature
+				MemoryStream memoryStream = new MemoryStream();
+				memoryStream.Write(saltValue, 0, saltValue.Length);
+				UTF8Encoding utf8Encoding = new UTF8Encoding();
+				byte[] legalNotice = utf8Encoding.GetBytes("Declaration of authentication intension.\n"
+					+ "The following data should be interpreted as an authentication challenge.\n");
+				memoryStream.Write(legalNotice, 0, legalNotice.Length);
+				byte[] challenge = (byte[]) httpContext.Session["challenge"];
+				memoryStream.Write(challenge, 0, challenge.Length);
+				memoryStream.Seek(0, SeekOrigin.Begin);
+				byte[] toBeSignedData = new byte[memoryStream.Length];
+				memoryStream.Read(toBeSignedData, 0, toBeSignedData.Length);
+				
+				RSACryptoServiceProvider rsaCryptoService = (RSACryptoServiceProvider)certificate2.PublicKey.Key;
+				
+				Log("to be signed data size: " + toBeSignedData.Length);
+				Log("signature value size: " + signatureValue.Length);
+				
+				bool signatureResult;
+				try {
+					signatureResult = rsaCryptoService.VerifyData(toBeSignedData, new SHA1CryptoServiceProvider(), signatureValue);
+					Log("signature result: " + signatureResult);
+				}
+				catch(Exception e) {
+					Log("error: " + e.Message);
+					httpResponse.StatusCode = 400; // bad request
+					return;
+				}
+				if (false == signatureResult) {
+					httpResponse.StatusCode = 400; // bad request
+					return;
+				}
 				
 				// TODO: verify certificate validity
 				
