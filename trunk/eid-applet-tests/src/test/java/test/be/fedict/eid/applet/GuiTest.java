8dfcd87252f412198e728e897184e785b7dc3d5c
frank.cornelis <frank.cornelis@24f8a504-ce5e-11dd-87b6-9d1a99559f42>
2009-07-23 12:10:41 +0000

    expermimented with the FEST test framework

---
+++ trunk/eid-applet-tests/src/test/java/test/be/fedict/eid/applet/GuiTest.java
@@ -0,0 +1,36 @@
+/*
+ * eID Applet Project.
+ * Copyright (C) 2009 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package test.be.fedict.eid.applet;
+
+import org.fest.swing.applet.AppletViewer;
+import org.fest.swing.fixture.FrameFixture;
+import org.fest.swing.launcher.AppletLauncher;
+import org.junit.Test;
+
+import be.fedict.eid.applet.Applet;
+
+public class GuiTest {
+
+	@Test
+	public void testAppletGUI() throws Exception {
+		AppletViewer appletViewer = AppletLauncher.applet(new Applet()).start();
+		FrameFixture applet = new FrameFixture(appletViewer);
+		applet.show();
+	}
+}
