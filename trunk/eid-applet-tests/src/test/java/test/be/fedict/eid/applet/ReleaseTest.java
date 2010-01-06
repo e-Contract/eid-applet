c2023640702aee332e7912e5b6281775a1c57187
frank.cornelis <frank.cornelis@24f8a504-ce5e-11dd-87b6-9d1a99559f42>
2010-01-06 09:24:30 +0000

    1.0.0-rc-3

--- trunk/eid-applet-tests/src/test/java/test/be/fedict/eid/applet/ReleaseTest.java
+++ trunk/eid-applet-tests/src/test/java/test/be/fedict/eid/applet/ReleaseTest.java
@@ -63,10 +63,10 @@ public class ReleaseTest {
 	private static final Log LOG = LogFactory.getLog(ReleaseTest.class);
 
 	// 1.0.0-SNAPSHOT
-	private static final String CURRENT_VERSION = "1.0.0-rc-2";
+	private static final String CURRENT_VERSION = "1.0.0-SNAPSHOT";
 
-	// 1.0.0-rc-1
-	private static final String NEW_VERSION = "1.0.0-SNAPSHOT";
+	// 1.0.0-rc-3
+	private static final String NEW_VERSION = "1.0.0-rc-3";
 
 	@Test
 	public void testVersioning() throws Exception {
