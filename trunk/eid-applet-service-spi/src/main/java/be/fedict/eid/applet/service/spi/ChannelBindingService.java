383ee184b35aaacc94906fae240ddaa1625da740
frank.cornelis <frank.cornelis@24f8a504-ce5e-11dd-87b6-9d1a99559f42>
2010-06-14 12:46:16 +0000

    documentation

--- trunk/eid-applet-service-spi/src/main/java/be/fedict/eid/applet/service/spi/ChannelBindingService.java
+++ trunk/eid-applet-service-spi/src/main/java/be/fedict/eid/applet/service/spi/ChannelBindingService.java
@@ -30,7 +30,9 @@ public interface ChannelBindingService {
 
 	/**
 	 * Gives back the X509 server SSL certificate that should be used for
-	 * verification of the secure channel binding.
+	 * verification of the secure channel binding. When <code>null</code> is
+	 * being returned the eID Applet Service can of course not perform the
+	 * secure channel binding verification.
 	 * 
 	 * @return the X509 server certificate.
 	 */
