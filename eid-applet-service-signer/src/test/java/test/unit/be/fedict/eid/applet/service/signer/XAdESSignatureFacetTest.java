8a2a40dedaef089ef04f96c48b999faa2ed0f038
Frank Cornelis <fcorneli@users.noreply.github.com>
2010-07-26 11:58:00 +0000

    documented the document types unit test for XAdES XML schema validation

--- eid-applet-service-signer/src/test/java/test/unit/be/fedict/eid/applet/service/signer/XAdESSignatureFacetTest.java
+++ eid-applet-service-signer/src/test/java/test/unit/be/fedict/eid/applet/service/signer/XAdESSignatureFacetTest.java
@@ -1,6 +1,6 @@
 /*
  * eID Applet Project.
- * Copyright (C) 2008-2009 FedICT.
+ * Copyright (C) 2008-2010 FedICT.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version
@@ -23,11 +23,15 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.security.KeyPair;
 import java.security.cert.X509Certificate;
 import java.util.Collections;
@@ -37,8 +41,16 @@ import javax.xml.crypto.KeySelector;
 import javax.xml.crypto.dsig.XMLSignature;
 import javax.xml.crypto.dsig.XMLSignatureFactory;
 import javax.xml.crypto.dsig.dom.DOMValidateContext;
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -51,6 +63,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.ls.LSInput;
+import org.w3c.dom.ls.LSResourceResolver;
 
 import be.fedict.eid.applet.service.signer.AbstractXmlSignatureService;
 import be.fedict.eid.applet.service.signer.SignatureFacet;
@@ -195,5 +209,151 @@ public class XAdESSignatureFacetTest {
 						"ds:Signature/ds:Object/xades:QualifyingProperties/xades:SignedProperties/xades:SignedSignatureProperties/xades:SigningCertificate/xades:Cert/xades:CertDigest/ds:DigestValue",
 						nsElement);
 		assertNotNull(resultNode);
+
+		// also test whether the XAdES extension is in line with the XAdES XML
+		// Schema.
+
+		// stax-api 1.0.1 prevents us from using
+		// "XMLConstants.W3C_XML_SCHEMA_NS_URI"
+		Node qualifyingPropertiesNode = XPathAPI.selectSingleNode(
+				signedDocument,
+				"ds:Signature/ds:Object/xades:QualifyingProperties", nsElement);
+		SchemaFactory factory = SchemaFactory
+				.newInstance("http://www.w3.org/2001/XMLSchema");
+		LSResourceResolver xadesResourceResolver = new XAdESLSResourceResolver();
+		factory.setResourceResolver(xadesResourceResolver);
+		InputStream schemaInputStream = XAdESSignatureFacetTest.class
+				.getResourceAsStream("/XAdESv141.xsd");
+		Source schemaSource = new StreamSource(schemaInputStream);
+		Schema schema = factory.newSchema(schemaSource);
+		Validator validator = schema.newValidator();
+		// DOMResult gives some DOMException...
+		validator.validate(new DOMSource(qualifyingPropertiesNode));
+
+		StreamSource streamSource = new StreamSource(tmpFile.toURI().toString());
+		ByteArrayOutputStream resultOutputStream = new ByteArrayOutputStream();
+		StreamResult streamResult = new StreamResult(resultOutputStream);
+		validator.validate(streamSource, streamResult);
+		LOG.debug("result: " + resultOutputStream);
+	}
+
+	private static class XAdESLSResourceResolver implements LSResourceResolver {
+
+		private static final Log LOG = LogFactory
+				.getLog(XAdESLSResourceResolver.class);
+
+		public LSInput resolveResource(String type, String namespaceURI,
+				String publicId, String systemId, String baseURI) {
+			LOG.debug("resolve resource");
+			LOG.debug("type: " + type);
+			LOG.debug("namespace URI: " + namespaceURI);
+			LOG.debug("publicId: " + publicId);
+			LOG.debug("systemId: " + systemId);
+			LOG.debug("base URI: " + baseURI);
+			if ("http://uri.etsi.org/01903/v1.3.2#".equals(namespaceURI)) {
+				return new LocalLSInput(publicId, systemId, baseURI,
+						"/XAdES.xsd");
+			}
+			if ("http://www.w3.org/2000/09/xmldsig#".equals(namespaceURI)) {
+				return new LocalLSInput(publicId, systemId, baseURI,
+						"/xmldsig-core-schema.xsd");
+			}
+			throw new RuntimeException("unsupported resource: " + namespaceURI);
+		}
+	}
+
+	private static class LocalLSInput implements LSInput {
+
+		private String publicId;
+
+		private String systemId;
+
+		private String baseURI;
+
+		private final String schemaResourceName;
+
+		public LocalLSInput(String publicId, String systemId, String baseURI,
+				String schemaResourceName) {
+			this.publicId = publicId;
+			this.systemId = systemId;
+			this.baseURI = baseURI;
+			this.schemaResourceName = schemaResourceName;
+		}
+
+		public String getBaseURI() {
+			return this.baseURI;
+		}
+
+		public InputStream getByteStream() {
+			InputStream inputStream = XAdESSignatureFacetTest.class
+					.getResourceAsStream(this.schemaResourceName);
+			return inputStream;
+		}
+
+		public boolean getCertifiedText() {
+			return true;
+		}
+
+		public Reader getCharacterStream() {
+			InputStream inputStream = getByteStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					inputStream));
+			return reader;
+		}
+
+		public String getEncoding() {
+			return "UTF-8";
+		}
+
+		public String getPublicId() {
+			return this.publicId;
+		}
+
+		public String getStringData() {
+			InputStream inputStream = getByteStream();
+			String stringData;
+			try {
+				stringData = IOUtils.toString(inputStream);
+			} catch (IOException e) {
+				throw new RuntimeException("I/O error: " + e.getMessage(), e);
+			}
+			return stringData;
+		}
+
+		public String getSystemId() {
+			return this.systemId;
+		}
+
+		public void setBaseURI(String baseURI) {
+			this.baseURI = baseURI;
+		}
+
+		public void setByteStream(InputStream byteStream) {
+			throw new UnsupportedOperationException();
+		}
+
+		public void setCertifiedText(boolean certifiedText) {
+			throw new UnsupportedOperationException();
+		}
+
+		public void setCharacterStream(Reader characterStream) {
+			throw new UnsupportedOperationException();
+		}
+
+		public void setEncoding(String encoding) {
+			throw new UnsupportedOperationException();
+		}
+
+		public void setPublicId(String publicId) {
+			this.publicId = publicId;
+		}
+
+		public void setStringData(String stringData) {
+			throw new UnsupportedOperationException();
+		}
+
+		public void setSystemId(String systemId) {
+			this.systemId = systemId;
+		}
 	}
 }
