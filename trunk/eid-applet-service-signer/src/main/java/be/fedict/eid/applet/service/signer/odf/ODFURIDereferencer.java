b7720ef64d9400427091d355ca8847686832755a
frank.cornelis <frank.cornelis@24f8a504-ce5e-11dd-87b6-9d1a99559f42>
2010-07-26 14:45:27 +0000

    ODT MathML DTD resolver

--- trunk/eid-applet-service-signer/src/main/java/be/fedict/eid/applet/service/signer/odf/ODFURIDereferencer.java
+++ trunk/eid-applet-service-signer/src/main/java/be/fedict/eid/applet/service/signer/odf/ODFURIDereferencer.java
@@ -21,7 +21,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-
 import java.net.URL;
 import java.net.URLDecoder;
 
@@ -32,9 +31,17 @@ import javax.xml.crypto.URIReference;
 import javax.xml.crypto.URIReferenceException;
 import javax.xml.crypto.XMLCryptoContext;
 import javax.xml.crypto.dsig.XMLSignatureFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.xml.security.signature.XMLSignatureInput;
+import org.w3c.dom.Document;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.SAXException;
 
 /**
  * JSR105 URI dereferencer implementation using an ODF file as resource
@@ -45,76 +52,112 @@ import org.apache.commons.logging.LogFactory;
  */
 public class ODFURIDereferencer implements URIDereferencer {
 
-    private static final Log LOG = LogFactory.getLog(ODFURIDereferencer.class);
-    private final URL odfUrl;
-    private final byte[] odfData;
-    private final URIDereferencer baseUriDereferener;
+	private static final Log LOG = LogFactory.getLog(ODFURIDereferencer.class);
+	private final URL odfUrl;
+	private final byte[] odfData;
+	private final URIDereferencer baseUriDereferener;
+	private final DocumentBuilder documentBuilder;
 
-    public ODFURIDereferencer(URL odfUrl) {
-        this(odfUrl, null);
-    }
+	public ODFURIDereferencer(URL odfUrl) {
+		this(odfUrl, null);
+	}
 
-    public ODFURIDereferencer(byte[] odfData) {
-        this(null, odfData);
-    }
+	public ODFURIDereferencer(byte[] odfData) {
+		this(null, odfData);
+	}
 
-    private ODFURIDereferencer(URL odfUrl, byte[] odfData) {
-        if (null == odfUrl && null == odfData) {
-            throw new IllegalArgumentException("odfUrl and odfData are null");
-        }
-        if (null != odfUrl && null != odfData) {
-            throw new IllegalArgumentException(
-                    "odfUrl and odfData are both not null");
-        }
-        this.odfUrl = odfUrl;
-        this.odfData = odfData;
-        XMLSignatureFactory xmlSignatureFactory = XMLSignatureFactory.getInstance();
-        this.baseUriDereferener = xmlSignatureFactory.getURIDereferencer();
-    }
+	private ODFURIDereferencer(URL odfUrl, byte[] odfData) {
+		if (null == odfUrl && null == odfData) {
+			throw new IllegalArgumentException("odfUrl and odfData are null");
+		}
+		if (null != odfUrl && null != odfData) {
+			throw new IllegalArgumentException(
+					"odfUrl and odfData are both not null");
+		}
+		this.odfUrl = odfUrl;
+		this.odfData = odfData;
+		XMLSignatureFactory xmlSignatureFactory = XMLSignatureFactory
+				.getInstance();
+		this.baseUriDereferener = xmlSignatureFactory.getURIDereferencer();
 
-    public Data dereference(URIReference uriReference, XMLCryptoContext context)
-            throws URIReferenceException {
-        if (null == uriReference) {
-            throw new NullPointerException("URIReference cannot be null");
-        }
-        if (null == context) {
-            throw new NullPointerException("XMLCrytoContext cannot be null");
-        }
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
+				.newInstance();
+		documentBuilderFactory.setNamespaceAware(true);
 
-        String uri = uriReference.getURI();
-        try {
-            uri = URLDecoder.decode(uri, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            LOG.warn("could not URL decode the uri: " + uri);
-        }
-        LOG.debug("dereference: " + uri);
-        try {
-            InputStream dataInputStream = findDataInputStream(uri);
-            if (null == dataInputStream) {
-                LOG.debug("cannot resolve, delegating to base DOM URI dereferener: " + uri);
-                return this.baseUriDereferener.dereference(uriReference,
-                        context);
-            }
-            return new OctetStreamData(dataInputStream, uri, null);
-        } catch (IOException e) {
-            throw new URIReferenceException("I/O error: " + e.getMessage(), e);
-        }
-    }
+		try {
+			this.documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			throw new RuntimeException(
+					"parser config error: " + e.getMessage(), e);
+		}
+		EntityResolver entityResolver = new ODTEntityResolver();
+		this.documentBuilder.setEntityResolver(entityResolver);
+	}
 
-    /**
-     * Find a given file / zip entry in the ODF package
-     *
-     * @param uri
-     * @return inputstream of the file
-     * @throws IOException
-     */
-    private InputStream findDataInputStream(String uri) throws IOException {
-        InputStream odfInputStream;
-        if (null != this.odfUrl) {
-            odfInputStream = this.odfUrl.openStream();
-        } else {
-            odfInputStream = new ByteArrayInputStream(this.odfData);
-        }
-        return ODFUtil.findDataInputStream(odfInputStream, uri);
-    }
+	public Data dereference(URIReference uriReference, XMLCryptoContext context)
+			throws URIReferenceException {
+		if (null == uriReference) {
+			throw new NullPointerException("URIReference cannot be null");
+		}
+		if (null == context) {
+			throw new NullPointerException("XMLCrytoContext cannot be null");
+		}
+
+		String uri = uriReference.getURI();
+		try {
+			uri = URLDecoder.decode(uri, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			LOG.warn("could not URL decode the uri: " + uri);
+		}
+		LOG.debug("dereference: " + uri);
+		try {
+			InputStream dataInputStream = findDataInputStream(uri);
+			if (null == dataInputStream) {
+				LOG
+						.debug("cannot resolve, delegating to base DOM URI dereferener: "
+								+ uri);
+				return this.baseUriDereferener.dereference(uriReference,
+						context);
+			}
+			if (uri.endsWith(".xml")) {
+				/*
+				 * We parse the XML ourselves as we might need to resolve MathML
+				 * DTD.
+				 */
+				byte[] data = IOUtils.toByteArray(dataInputStream);
+				if (0 == data.length) {
+					return new OctetStreamData(dataInputStream, uri, null);
+				}
+				Document document = documentBuilder
+						.parse(new ByteArrayInputStream(data));
+				XMLSignatureInput xmlSignatureInput = new XMLSignatureInput(
+						document);
+				ApacheNodeSetData apacheNodeSetData = new ApacheNodeSetData(
+						xmlSignatureInput);
+				return apacheNodeSetData;
+			}
+			return new OctetStreamData(dataInputStream, uri, null);
+		} catch (IOException e) {
+			throw new URIReferenceException("I/O error: " + e.getMessage(), e);
+		} catch (SAXException e) {
+			throw new URIReferenceException("SAX error: " + e.getMessage(), e);
+		}
+	}
+
+	/**
+	 * Find a given file / zip entry in the ODF package
+	 * 
+	 * @param uri
+	 * @return inputstream of the file
+	 * @throws IOException
+	 */
+	private InputStream findDataInputStream(String uri) throws IOException {
+		InputStream odfInputStream;
+		if (null != this.odfUrl) {
+			odfInputStream = this.odfUrl.openStream();
+		} else {
+			odfInputStream = new ByteArrayInputStream(this.odfData);
+		}
+		return ODFUtil.findDataInputStream(odfInputStream, uri);
+	}
 }
--- trunk/eid-applet-service-signer/src/main/java/be/fedict/eid/applet/service/signer/odf/ODFUtil.java
+++ trunk/eid-applet-service-signer/src/main/java/be/fedict/eid/applet/service/signer/odf/ODFUtil.java
@@ -49,205 +49,211 @@ import org.apache.commons.logging.LogFactory;
 
 /**
  * Helper class to remove some code duplication
- *
+ * 
  * @author Bart Hanssens
  */
 public class ODFUtil {
-    public static String MANIFEST_FILE = "META-INF/manifest.xml";
-    public static String MIMETYPE_FILE = "mimetype";
-    public static String MIMETYPE_START = "application/vnd.oasis.opendocument";
-    public static String SIGNATURE_FILE = "META-INF/documentsignatures.xml";
-    public static String SIGNATURE_NS = 
-            "urn:oasis:names:tc:opendocument:xmlns:digitalsignature:1.0";
-    public static String SIGNATURE_ELEMENT = "document-signatures";
+	public static String MANIFEST_FILE = "META-INF/manifest.xml";
+	public static String MIMETYPE_FILE = "mimetype";
+	public static String MIMETYPE_START = "application/vnd.oasis.opendocument";
+	public static String SIGNATURE_FILE = "META-INF/documentsignatures.xml";
+	public static String SIGNATURE_NS = "urn:oasis:names:tc:opendocument:xmlns:digitalsignature:1.0";
+	public static String SIGNATURE_ELEMENT = "document-signatures";
 
-    private static final Log LOG = LogFactory.getLog(ODFUtil.class);
+	private static final Log LOG = LogFactory.getLog(ODFUtil.class);
 
-     /**
-     * Load an XML file from ODF package as a DOM Document
-     *
-     * @param documentInputStream
-     * @return
-     * @throws ParserConfigurationException
-     * @throws SAXException
-     * @throws IOException
-     */
-    public static Document loadDocument(InputStream documentInputStream)
-            throws ParserConfigurationException, SAXException, IOException {
-        DocumentBuilder documentBuilder = getNewDocumentBuilder();
-        Document document = documentBuilder.parse(documentInputStream);
-        return document;
-    }
-
-    /**
-     * Return a new DOM Document
-     *
-     * @return DOM Document
-     * @throws ParserConfigurationException
-     */
-    public static Document getNewDocument() throws ParserConfigurationException {
-        return getNewDocumentBuilder().newDocument();
-    }
-
-    /**
-     * Return a new DOM Document Builder
-     *
-     * @return DOM Document Builder
-     * @throws ParserConfigurationException
-     */
-    public static DocumentBuilder getNewDocumentBuilder() throws ParserConfigurationException {
-        LOG.debug("new DOM document builder");
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        documentBuilderFactory.setNamespaceAware(true);
-        return documentBuilderFactory.newDocumentBuilder();
-    }
-
-    /**
-     * Read the zipped data in the ODF package and return the inputstream
-     * for a given file / zip entry
-     *
-     * @param inputStream
-     * @param uri
-     * @return inputstream for the file / zip entry
-     * @throws IOException
-     */
-    protected static InputStream findDataInputStream(InputStream inputStream,
-            String uri) throws IOException {
-        ZipInputStream zipInputStream = new ZipInputStream(inputStream);
-        ZipEntry zipEntry;
-        while (null != (zipEntry = zipInputStream.getNextEntry())) {
-            if (zipEntry.getName().equals(uri)) {
-                return zipInputStream;
-            }
+	/**
+	 * Load an XML file from ODF package as a DOM Document
+	 * 
+	 * @param documentInputStream
+	 * @return
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 */
+	public static Document loadDocument(InputStream documentInputStream)
+			throws ParserConfigurationException, SAXException, IOException {
+		DocumentBuilder documentBuilder = getNewDocumentBuilder();
+		Document document = documentBuilder.parse(documentInputStream);
+		return document;
 	}
-	return null;
-    }
 
+	/**
+	 * Return a new DOM Document
+	 * 
+	 * @return DOM Document
+	 * @throws ParserConfigurationException
+	 */
+	public static Document getNewDocument() throws ParserConfigurationException {
+		return getNewDocumentBuilder().newDocument();
+	}
 
-    /**
-     * Checks if a file / zip entry is a content file
-     *
-     * @param zipEntry
-     * @return true if zip entry is a content file
-     */
-    protected static boolean isContentFile(ZipEntry zipEntry) {
-        /* should be enough for most simple ODF files and ODF embedded into
-         another ODF file (e.g. chart in spreadsheet) */
-        return zipEntry.getName().endsWith("content.xml");
-    }
+	/**
+	 * Return a new DOM Document Builder
+	 * 
+	 * @return DOM Document Builder
+	 * @throws ParserConfigurationException
+	 */
+	public static DocumentBuilder getNewDocumentBuilder()
+			throws ParserConfigurationException {
+		LOG.debug("new DOM document builder");
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
+				.newInstance();
+		documentBuilderFactory.setNamespaceAware(true);
+		return documentBuilderFactory.newDocumentBuilder();
+	}
 
-    /**
-     * Checks if a file / zip entry is a signature file
-     *
-     * @param zipEntry
-     * @return true if zip entry is a signature file
-     */
-    protected static boolean isSignatureFile(ZipEntry zipEntry) {
-        /* for now, only check for document signatures,
-           not the other macro signatures or application specific signatures */
-        return zipEntry.getName().equals(SIGNATURE_FILE);
-    }
+	/**
+	 * Read the zipped data in the ODF package and return the inputstream for a
+	 * given file / zip entry
+	 * 
+	 * @param inputStream
+	 * @param uri
+	 * @return inputstream for the file / zip entry
+	 * @throws IOException
+	 */
+	protected static InputStream findDataInputStream(InputStream inputStream,
+			String uri) throws IOException {
+		ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+		ZipEntry zipEntry;
+		while (null != (zipEntry = zipInputStream.getNextEntry())) {
+			if (zipEntry.getName().equals(uri)) {
+				return zipInputStream;
+			}
+		}
+		return null;
+	}
 
+	/**
+	 * Checks if a file / zip entry is a content file
+	 * 
+	 * @param zipEntry
+	 * @return true if zip entry is a content file
+	 */
+	protected static boolean isContentFile(ZipEntry zipEntry) {
+		/*
+		 * should be enough for most simple ODF files and ODF embedded into
+		 * another ODF file (e.g. chart in spreadsheet)
+		 */
+		return zipEntry.getName().endsWith("content.xml");
+	}
 
-    /**
-     * Check if a file / zip entry is to be signed
-     *
-     * @param zipEntry
-     * @return true if zip entry is to be signed
-     */
-    protected static boolean isToBeSigned(ZipEntry zipEntry) {
-        String name = zipEntry.getName();
+	/**
+	 * Checks if a file / zip entry is a signature file
+	 * 
+	 * @param zipEntry
+	 * @return true if zip entry is a signature file
+	 */
+	protected static boolean isSignatureFile(ZipEntry zipEntry) {
+		/*
+		 * for now, only check for document signatures, not the other macro
+		 * signatures or application specific signatures
+		 */
+		return zipEntry.getName().equals(SIGNATURE_FILE);
+	}
 
-        /* OOo 3.0/3.1 bug: don't sign mimetype stream nor the manifest */
-        /* if (zipEntry.isDirectory() ||
-             name.equals(MIMETYPE_FILE) ||
-             name.equals(MANIFEST_FILE) || */
-        /* Corrected in OOo 3.2 */
+	/**
+	 * Check if a file / zip entry is to be signed
+	 * 
+	 * @param zipEntry
+	 * @return true if zip entry is to be signed
+	 */
+	protected static boolean isToBeSigned(ZipEntry zipEntry) {
+		String name = zipEntry.getName();
 
-        if (zipEntry.isDirectory() || name.equals(SIGNATURE_FILE)) {
-            return false;
-        }
-        return true;
-    }
+		/* OOo 3.0/3.1 bug: don't sign mimetype stream nor the manifest */
+		/*
+		 * if (zipEntry.isDirectory() || name.equals(MIMETYPE_FILE) ||
+		 * name.equals(MANIFEST_FILE) ||
+		 */
+		/* Corrected in OOo 3.2 */
 
-    /**
-     * Get a list of all the files / zip entries in an ODF package 
-     * @param odfUrl
-     * @return
-     * @throws IOException
-     */
-    protected static List getZipEntriesAsList(InputStream odfInputStream) throws IOException {
-        ArrayList list = new ArrayList();
+		if (zipEntry.isDirectory() || name.equals(SIGNATURE_FILE)) {
+			return false;
+		}
+		return true;
+	}
 
-        ZipInputStream odfZipInputStream = new ZipInputStream(odfInputStream);
-        ZipEntry zipEntry;
+	/**
+	 * Get a list of all the files / zip entries in an ODF package
+	 * 
+	 * @param odfUrl
+	 * @return
+	 * @throws IOException
+	 */
+	protected static List getZipEntriesAsList(InputStream odfInputStream)
+			throws IOException {
+		ArrayList list = new ArrayList();
 
-        while (null != (zipEntry = odfZipInputStream.getNextEntry())) {
-            list.add(zipEntry.getName());
-        }
-        return list;
-    }
+		ZipInputStream odfZipInputStream = new ZipInputStream(odfInputStream);
+		ZipEntry zipEntry;
 
-    /**
-     * Check if an ODF package is self-contained, i.e. content files don't have 
-     * OLE objects linked to external files
-     *
-     * @param odfUrl
-     * @return
-     * @throws IOException
-     * @throws ParserConfigurationException
-     * @throws SAXException
-     * @throws XPathExpressionException
-     */
-    public static boolean isSelfContained(URL odfUrl)
-            throws IOException, ParserConfigurationException, SAXException, XPathExpressionException {
-        InputStream odfInputStream = odfUrl.openStream();
-        List zipEntries = getZipEntriesAsList(odfInputStream);
+		while (null != (zipEntry = odfZipInputStream.getNextEntry())) {
+			list.add(zipEntry.getName());
+		}
+		return list;
+	}
 
-        odfInputStream = odfUrl.openStream();
-        ZipInputStream odfZipInputStream = new ZipInputStream(odfInputStream);
-        ZipEntry zipEntry;
+	/**
+	 * Check if an ODF package is self-contained, i.e. content files don't have
+	 * OLE objects linked to external files
+	 * 
+	 * @param odfUrl
+	 * @return
+	 * @throws IOException
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws XPathExpressionException
+	 */
+	public static boolean isSelfContained(URL odfUrl) throws IOException,
+			ParserConfigurationException, SAXException,
+			XPathExpressionException {
+		InputStream odfInputStream = odfUrl.openStream();
+		List zipEntries = getZipEntriesAsList(odfInputStream);
 
-        XPathFactory factory = XPathFactory.newInstance();
-        /* Maybe a bit overkill, but implementations can use other prefixes */
-        ODFNamespaceContext namespaceContext = new ODFNamespaceContext();
+		odfInputStream = odfUrl.openStream();
+		ZipInputStream odfZipInputStream = new ZipInputStream(odfInputStream);
+		ZipEntry zipEntry;
 
-        XPath xpath = factory.newXPath();
-        xpath.setNamespaceContext(namespaceContext);
-        XPathExpression expression =  xpath.compile(
-                "//draw:object/@xlink:href|" +
-                "//draw:object-ole/@xlink:href|" +
-                "//draw:image/@xlink:href|" +
-                "//draw:floating-frame/@xlink:href");
+		XPathFactory factory = XPathFactory.newInstance();
+		/* Maybe a bit overkill, but implementations can use other prefixes */
+		ODFNamespaceContext namespaceContext = new ODFNamespaceContext();
 
-        while (null != (zipEntry = odfZipInputStream.getNextEntry())) {
-            if (isContentFile(zipEntry)) {
-                /* TODO: pure SAX is probably more memory-efficient */
-                Document content = ODFUtil.loadDocument(odfZipInputStream);
-                NodeList nodes = (NodeList)
-                        expression.evaluate(content, XPathConstants.NODESET);
-                return checkNodes(nodes, zipEntries);
-            }
-        }
-        return true;
-    }
+		XPath xpath = factory.newXPath();
+		xpath.setNamespaceContext(namespaceContext);
+		XPathExpression expression = xpath.compile("//draw:object/@xlink:href|"
+				+ "//draw:object-ole/@xlink:href|"
+				+ "//draw:image/@xlink:href|"
+				+ "//draw:floating-frame/@xlink:href");
 
-    protected static boolean checkNodes(NodeList nodes, List zipEntries) {
-        for (int i = 0; i < nodes.getLength(); i++) {
-            String url = nodes.item(i).getNodeValue();
-            if ("".equals(url)) {
-                LOG.debug("Skip empty xlink:href");
-                continue;
-            }
-            if (url.startsWith("./")) {
-                url = url.substring(2);
-            }
-            /* check if inside package or not */
-            if (! zipEntries.contains(url)) {
-                LOG.debug("Not self-contained: " + url + " outside package");
-                return false;
-            }
-        }
-        return true;
-    }
+		while (null != (zipEntry = odfZipInputStream.getNextEntry())) {
+			if (isContentFile(zipEntry)) {
+				/* TODO: pure SAX is probably more memory-efficient */
+				Document content = ODFUtil.loadDocument(odfZipInputStream);
+				NodeList nodes = (NodeList) expression.evaluate(content,
+						XPathConstants.NODESET);
+				return checkNodes(nodes, zipEntries);
+			}
+		}
+		return true;
+	}
+
+	protected static boolean checkNodes(NodeList nodes, List zipEntries) {
+		for (int i = 0; i < nodes.getLength(); i++) {
+			String url = nodes.item(i).getNodeValue();
+			if ("".equals(url)) {
+				LOG.debug("Skip empty xlink:href");
+				continue;
+			}
+			if (url.startsWith("./")) {
+				url = url.substring(2);
+			}
+			/* check if inside package or not */
+			if (!zipEntries.contains(url)) {
+				LOG.debug("Not self-contained: " + url + " outside package");
+				return false;
+			}
+		}
+		return true;
+	}
 }
