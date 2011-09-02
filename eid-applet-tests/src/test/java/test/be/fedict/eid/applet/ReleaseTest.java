347402c05a6f9d9cb438023ef52aec31e981d721
Frank Cornelis <fcorneli@users.noreply.github.com>
2011-09-02 11:51:40 +0000

    preparing for next development cycle

--- eid-applet-tests/src/test/java/test/be/fedict/eid/applet/ReleaseTest.java
+++ eid-applet-tests/src/test/java/test/be/fedict/eid/applet/ReleaseTest.java
@@ -46,7 +46,6 @@ import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import com.sun.org.apache.xpath.internal.XPathAPI;
@@ -63,7 +62,7 @@ public class ReleaseTest {
 	private static final Log LOG = LogFactory.getLog(ReleaseTest.class);
 
 	// 1.0.2-SNAPSHOT
-	private static final String CURRENT_VERSION = "1.0.4.Beta2";
+	private static final String CURRENT_VERSION = "1.0.4.Beta3";
 
 	// 1.0.2.Beta2
 	private static final String NEW_VERSION = "1.0.4-SNAPSHOT";
