df9f8e02f778bb98e635c19d6732bee2348f4cdc
frank.cornelis <frank.cornelis@24f8a504-ce5e-11dd-87b6-9d1a99559f42>
2012-07-05 09:57:12 +0000

    CDI: removed dependency on JBoss Seam injection

--- trunk/eid-applet-service-cdi/src/main/java/be/fedict/eid/applet/service/cdi/BelgianIdentityCardProducer.java
+++ trunk/eid-applet-service-cdi/src/main/java/be/fedict/eid/applet/service/cdi/BelgianIdentityCardProducer.java
@@ -1,10 +1,11 @@
 package be.fedict.eid.applet.service.cdi;
 
 import java.security.cert.X509Certificate;
+import java.util.Map;
 
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
-import javax.servlet.http.HttpSession;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 import be.fedict.eid.applet.service.Address;
 import be.fedict.eid.applet.service.Identity;
@@ -12,59 +13,65 @@ import be.fedict.eid.applet.service.cdi.BelgianCertificate.CERTIFICATE_TYPE;
 import be.fedict.eid.applet.service.impl.handler.IdentityDataMessageHandler;
 
 /**
- * CDI producer of eID Applet Service data.
+ * CDI/JSF producer of eID Applet Service data.
  * 
  * @author Frank Cornelis
  * 
  */
 public class BelgianIdentityCardProducer {
 
-	@Inject
-	private HttpSession httpSession;
-
 	@Produces
 	public Identity createIdentity() {
-		Identity identity = (Identity) this.httpSession
-				.getAttribute(IdentityDataMessageHandler.IDENTITY_SESSION_ATTRIBUTE);
+		Identity identity = getSessionAttribute(Identity.class,
+				IdentityDataMessageHandler.IDENTITY_SESSION_ATTRIBUTE);
 		return identity;
 	}
 
 	@Produces
 	public Address createAddress() {
-		Address address = (Address) this.httpSession
-				.getAttribute(IdentityDataMessageHandler.ADDRESS_SESSION_ATTRIBUTE);
+		Address address = getSessionAttribute(Address.class,
+				IdentityDataMessageHandler.ADDRESS_SESSION_ATTRIBUTE);
 		return address;
 	}
 
 	@Produces
 	@BelgianCertificate(CERTIFICATE_TYPE.AUTH)
 	public X509Certificate createAuthCert() {
-		X509Certificate cert = (X509Certificate) this.httpSession
-				.getAttribute(IdentityDataMessageHandler.AUTHN_CERT_SESSION_ATTRIBUTE);
+		X509Certificate cert = getSessionAttribute(X509Certificate.class,
+				IdentityDataMessageHandler.AUTHN_CERT_SESSION_ATTRIBUTE);
 		return cert;
 	}
 
 	@Produces
 	@BelgianCertificate(CERTIFICATE_TYPE.SIGN)
 	public X509Certificate createSignCert() {
-		X509Certificate cert = (X509Certificate) this.httpSession
-				.getAttribute(IdentityDataMessageHandler.SIGN_CERT_SESSION_ATTRIBUTE);
+		X509Certificate cert = getSessionAttribute(X509Certificate.class,
+				IdentityDataMessageHandler.SIGN_CERT_SESSION_ATTRIBUTE);
 		return cert;
 	}
 
 	@Produces
 	@BelgianCertificate(CERTIFICATE_TYPE.CITIZEN_CA)
 	public X509Certificate createCitizenCACert() {
-		X509Certificate cert = (X509Certificate) this.httpSession
-				.getAttribute(IdentityDataMessageHandler.CA_CERT_SESSION_ATTRIBUTE);
+		X509Certificate cert = getSessionAttribute(X509Certificate.class,
+				IdentityDataMessageHandler.CA_CERT_SESSION_ATTRIBUTE);
 		return cert;
 	}
 
 	@Produces
 	@BelgianCertificate(CERTIFICATE_TYPE.ROOT_CA)
 	public X509Certificate createRootCACert() {
-		X509Certificate cert = (X509Certificate) this.httpSession
-				.getAttribute(IdentityDataMessageHandler.ROOT_CERT_SESSION_ATTRIBUTE);
+		X509Certificate cert = getSessionAttribute(X509Certificate.class,
+				IdentityDataMessageHandler.ROOT_CERT_SESSION_ATTRIBUTE);
 		return cert;
+	}
+
+	private <T> T getSessionAttribute(Class<? extends T> clazz,
+			String attributeName) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = facesContext.getExternalContext();
+		Map<String, Object> sessionMap = externalContext.getSessionMap();
+		T sessionAttribute = (T) sessionMap.get(attributeName);
+		return sessionAttribute;
 	}
 }
