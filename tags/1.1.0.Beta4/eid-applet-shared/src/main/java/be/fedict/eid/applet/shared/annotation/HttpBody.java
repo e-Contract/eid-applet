99ada009958712f6f945d1c7a0f6fbea702bfbc4
frank.cornelis <frank.cornelis@24f8a504-ce5e-11dd-87b6-9d1a99559f42>
2013-01-23 11:52:52 +0000

    tagging 1.1.0.Beta4

---
+++ tags/1.1.0.Beta4/eid-applet-shared/src/main/java/be/fedict/eid/applet/shared/annotation/HttpBody.java
@@ -0,0 +1,37 @@
+/*
+ * eID Applet Project.
+ * Copyright (C) 2008-2009 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.fedict.eid.applet.shared.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks that a field will be transmitted as the HTTP body.
+ * 
+ * @author Frank Cornelis
+ * 
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Documented
+public @interface HttpBody {
+}
---
+++ tags/1.1.0.Beta4/eid-applet-test/eid-applet-test-webapp/src/main/webapp/authenticate-untrusted.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<html>
+<head>
+<title>eID Applet Authentication Demo</title>
+</head>
+<body>
+<h1>eID Applet Authentication Demo</h1>
+<p>Simulates untrusted eID certificates.</p>
+<script src="https://www.java.com/js/deployJava.js"></script>
+<script>
+	var attributes = {
+		code :'be.fedict.eid.applet.Applet.class',
+		archive :'eid-applet-package-${pom.version}.jar',
+		width :600,
+		height :300
+	};
+	var parameters = {
+		TargetPage :'authn-result.jsp',
+		AppletService :'applet-service-authn-untrusted',
+		BackgroundColor :'#ffffff',
+		Language : 'en'
+	};
+	var version = '1.6';
+	deployJava.runApplet(attributes, parameters, version);
+</script>
+</body>
+</html>
