aa7d764afcf34ef08702a67a15f0ae0ad4aa2d5f
Wim Vandenhaute <wvdhaute@users.noreply.github.com>
2013-11-04 14:19:45 +0000

    Use XPath 2 filter for cosignature
    XPath v1 results in very bad performance during c14n

--- eid-applet-service-signer/src/main/java/be/fedict/eid/applet/service/signer/facets/CoSignatureFacet.java
+++ eid-applet-service-signer/src/main/java/be/fedict/eid/applet/service/signer/facets/CoSignatureFacet.java
@@ -18,28 +18,19 @@
 
 package be.fedict.eid.applet.service.signer.facets;
 
-import java.security.InvalidAlgorithmParameterException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-import javax.xml.crypto.dsig.CanonicalizationMethod;
-import javax.xml.crypto.dsig.DigestMethod;
-import javax.xml.crypto.dsig.Reference;
-import javax.xml.crypto.dsig.Transform;
-import javax.xml.crypto.dsig.XMLObject;
-import javax.xml.crypto.dsig.XMLSignatureFactory;
-import javax.xml.crypto.dsig.spec.TransformParameterSpec;
-import javax.xml.crypto.dsig.spec.XPathFilterParameterSpec;
-
+import be.fedict.eid.applet.service.signer.DigestAlgo;
+import be.fedict.eid.applet.service.signer.SignatureFacet;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-import be.fedict.eid.applet.service.signer.DigestAlgo;
-import be.fedict.eid.applet.service.signer.SignatureFacet;
+import javax.xml.crypto.dsig.*;
+import javax.xml.crypto.dsig.spec.TransformParameterSpec;
+import javax.xml.crypto.dsig.spec.XPathFilter2ParameterSpec;
+import javax.xml.crypto.dsig.spec.XPathType;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.util.*;
 
 /**
  * Signature facet to create multiple independent signatures, a.k.a
@@ -95,14 +86,24 @@ public class CoSignatureFacet implements SignatureFacet {
 				this.digestAlgo.getXmlAlgoId(), null);
 
 		List<Transform> transforms = new LinkedList<Transform>();
-		Map<String, String> xpathNamespaceMap = new HashMap<String, String>();
-		xpathNamespaceMap.put("ds", "http://www.w3.org/2000/09/xmldsig#");
-		Transform envelopedTransform = signatureFactory.newTransform(
-				CanonicalizationMethod.XPATH, new XPathFilterParameterSpec(
-						"not(ancestor-or-self::ds:Signature)",
-						xpathNamespaceMap));
-		transforms.add(envelopedTransform);
-		Transform exclusiveTransform = signatureFactory
+        Map<String, String> xpathNamespaceMap = new HashMap<String, String>();
+        xpathNamespaceMap.put("ds", "http://www.w3.org/2000/09/xmldsig#");
+
+        // XPath v1 - slow...
+//		Transform envelopedTransform = signatureFactory.newTransform(
+//				CanonicalizationMethod.XPATH, new XPathFilterParameterSpec(
+//						"not(ancestor-or-self::ds:Signature)",
+//						xpathNamespaceMap));
+
+        // XPath v2 - fast...
+        List<XPathType> types = new ArrayList<XPathType>(1);
+        types.add(new XPathType("/descendant::*[name()='ds:Signature']", XPathType.Filter.SUBTRACT, xpathNamespaceMap));
+        Transform envelopedTransform = signatureFactory.newTransform(
+                CanonicalizationMethod.XPATH2, new XPathFilter2ParameterSpec(types));
+
+        transforms.add(envelopedTransform);
+
+        Transform exclusiveTransform = signatureFactory
 				.newTransform(CanonicalizationMethod.EXCLUSIVE,
 						(TransformParameterSpec) null);
 		transforms.add(exclusiveTransform);
